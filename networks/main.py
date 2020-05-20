868a2503410b2e53c09900f359b0829be96a0cae
unknown <joasiawozna@gmail.com>
2020-05-20 17:43:42 +0200

    Add voice commands example

--- networks/main.py
+++ networks/main.py
@@ -1,10 +1,30 @@
 from network_tools.utils import Util, TimeHistory
 from keras.callbacks import CSVLogger, ModelCheckpoint, LearningRateScheduler, EarlyStopping, TensorBoard
-from network_tools.data import DataGenerator
+from network_tools.data import DataGenerator, DataGeneratorSimple
 from network_tools.networks import NetworkInceptionV3
 from network_tools.architecture import Architecture
 from network_tools.settings import Setting
 from contextlib import redirect_stdout
+
+# Scientific Math
+import numpy as np
+from scipy.fftpack import fft
+from scipy import signal
+from scipy.io import wavfile
+from sklearn.model_selection import train_test_split
+
+#Deep learning
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras import Input, layers
+from tensorflow.keras import backend as K
+
+# Visualization
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
 
 
 class Teacher:
@@ -53,35 +73,130 @@ class Teacher:
         setting = Setting()
 
         # data generate
-        data = DataGenerator(self.__base_dir, setting)
+        data = DataGeneratorSimple()
+        data.preprocessing()
 
-        # create network and get model
-        network = NetworkInceptionV3(setting, data)
-        model = network.create_model()
+        train_wav, test_wav, train_label, test_label = train_test_split(data.get_wav_vals(), data.get_label_vals(),
+                                                                        test_size=0.2,
+                                                                        random_state=1993,
+                                                                        shuffle=True)
 
-        # create util for model, logging
-        architecture = Architecture(model, self.__model_path)
-        architecture.log()
+        # Parameters
+        lr = 0.001
+        generations = 20000
+        num_gens_to_wait = 250
+        batch_size = 512
+        drop_out_rate = 0.5
+        input_shape = (8000, 1)
 
-        # save settings and layers
-        Util.save_setting(self.__model_path, setting, self.__epochs)
-        with open(self.__model_path + 'model_summary.txt', 'w') as f:
-            with redirect_stdout(f):
-                model.summary()  # save network layers
+        # For Conv1D add Channel
+        train_wav = train_wav.reshape(-1, 8000, 1)
+        test_wav = test_wav.reshape(-1, 8000, 1)
 
-        callbacks = self.__get_callbacks()
+        label_value = data.get_target_list()
+        label_value.append('unknown')
+        label_value.append('silence')
 
-        # train and validation
-        history = network.fit(epochs=self.__epochs, callbacks=callbacks)
+        new_label_value = dict()
+        for i, l in enumerate(label_value):
+            new_label_value[l] = i
+        label_value = new_label_value
 
-        # test
-        test_loss, test_acc = network.evaluate()
-        print('test acc:', test_acc)
+        # Make Label data 'string' -> 'class num'
+        temp = []
+        for v in train_label:
+            temp.append(label_value[v[0]])
+        train_label = np.array(temp)
 
-        # save model, please check file name
-        Util.save_model(model, self.__path_name, test_acc)
-        Util.save_history(self.__model_path, history)
-        Util.save_time(self.__model_path, callbacks[4])
+        temp = []
+        for v in test_label:
+            temp.append(label_value[v[0]])
+        test_label = np.array(temp)
+
+        # Make Label data 'class num' -> 'One hot vector'
+        train_label = keras.utils.to_categorical(train_label, len(label_value))
+        test_label = keras.utils.to_categorical(test_label, len(label_value))
+
+        print('Train_Wav Demension : ' + str(np.shape(train_wav)))
+
+        # Conv1D Model
+        input_tensor = Input(shape=(input_shape))
+
+        x = layers.Conv1D(8, 11, padding='valid', activation='relu', strides=1)(input_tensor)
+        x = layers.MaxPooling1D(2)(x)
+        x = layers.Dropout(drop_out_rate)(x)
+        x = layers.Conv1D(16, 7, padding='valid', activation='relu', strides=1)(x)
+        x = layers.MaxPooling1D(2)(x)
+        x = layers.Dropout(drop_out_rate)(x)
+        x = layers.Conv1D(32, 5, padding='valid', activation='relu', strides=1)(x)
+        x = layers.MaxPooling1D(2)(x)
+        x = layers.Dropout(drop_out_rate)(x)
+        x = layers.Conv1D(64, 5, padding='valid', activation='relu', strides=1)(x)
+        x = layers.MaxPooling1D(2)(x)
+        x = layers.Dropout(drop_out_rate)(x)
+        x = layers.Conv1D(128, 3, padding='valid', activation='relu', strides=1)(x)
+        x = layers.MaxPooling1D(2)(x)
+        x = layers.Flatten()(x)
+        x = layers.Dense(256, activation='relu')(x)
+        x = layers.Dropout(drop_out_rate)(x)
+        x = layers.Dense(128, activation='relu')(x)
+        x = layers.Dropout(drop_out_rate)(x)
+        output_tensor = layers.Dense(len(label_value), activation='softmax')(x)
+
+        model = tf.keras.Model(input_tensor, output_tensor)
+
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adam(lr=lr),
+                      metrics=['accuracy'])
+
+        history = model.fit(train_wav, train_label, validation_data=[test_wav, test_label],
+                            batch_size=batch_size,
+                            epochs=100,
+                            verbose=1)
+
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+        # # create network and get model
+        # network = NetworkInceptionV3(setting, data)
+        # model = network.create_model()
+        #
+        # # create util for model, logging
+        # architecture = Architecture(model, self.__model_path)
+        # architecture.log()
+        #
+        # # save settings and layers
+        # Util.save_setting(self.__model_path, setting, self.__epochs)
+        # with open(self.__model_path + 'model_summary.txt', 'w') as f:
+        #     with redirect_stdout(f):
+        #         model.summary()  # save network layers
+        #
+        # callbacks = self.__get_callbacks()
+        #
+        # # train and validation
+        # history = network.fit(epochs=self.__epochs, callbacks=callbacks)
+        #
+        # # test
+        # test_loss, test_acc = network.evaluate()
+        # print('test acc:', test_acc)
+        #
+        # # save model, please check file name
+        # Util.save_model(model, self.__path_name, test_acc)
+        # Util.save_history(self.__model_path, history)
+        # Util.save_time(self.__model_path, callbacks[4])
 
 
 if __name__ == "__main__":
--- networks/network_tools/data.py
+++ networks/network_tools/data.py
@@ -1,6 +1,24 @@
+# path
 import os
-from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.inception_v3 import preprocess_input  # change model
+from os.path import isdir, join
+from pathlib import Path
+
+# Scientific Math
+import numpy as np
+from scipy.fftpack import fft
+from scipy import signal
+from scipy.io import wavfile
+from sklearn.model_selection import train_test_split
+
+# Deep learning
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras import Input, layers
+from tensorflow.keras import backend as K
+
+import random
+import copy
+import librosa
 
 
 class DataGenerator:
@@ -61,3 +79,134 @@ class DataGenerator:
 
     def batch_size(self):
         return self.__batch_size
+
+
+class DataGeneratorSimple:
+    def __init__(self):
+
+        self.__train_audio_path = 'D:\\a__a\\projects\\data\\train\\audio'
+        self.__dirs = [f for f in os.listdir(self.__train_audio_path) if isdir(join(self.__train_audio_path, f))]
+        self.__dirs.sort()
+        self.__max_ratio = 0.1
+        print('Number of labels: ' + str(len(self.__dirs[1:])))
+        print(self.__dirs)
+        self.__all_wav = []
+        self.__unknown_wav = []
+        self.__noised_wav = []
+        self.__wav_vals = []
+        self.__label_all = []
+        self.__label_value = {}
+        self.__labels = []
+        self.__unknown = []
+        self.__silence_wav = []
+        self.__silence_label = []
+        self.__target_list = ['yes', 'no']   #, 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go']
+        self.__unknown_list = [d for d in self.__dirs if d not in self.__target_list and d != '_background_noise_']
+        print('target_list : ', end='')
+        print(self.__target_list)
+        print('unknowns_list : ', end='')
+        print(self.__unknown_list)
+        print('silence : _background_noise_')
+        i = 0;
+        self.__background = [f for f in os.listdir(join(self.__train_audio_path, '_background_noise_')) if
+                             f.endswith('.wav')]
+        self.__background_noise = []
+        for wav in self.__background:
+            samples, sample_rate = librosa.load(join(join(self.__train_audio_path, '_background_noise_'), wav))
+            samples = librosa.resample(samples, sample_rate, 8000)
+            self.__background_noise.append(samples)
+
+        for direct in self.__dirs[1:]:
+            waves = [f for f in os.listdir(join(self.__train_audio_path, direct)) if f.endswith('.wav')]
+            self.__label_value[direct] = i
+            i = i + 1
+            print(str(i) + ":" + str(direct) + " ", end="")
+            for wav in waves:
+                samples, sample_rate = librosa.load(join(join(self.__train_audio_path, direct), wav), sr=16000)
+                samples = librosa.resample(samples, sample_rate, 8000)
+                if len(samples) != 8000:
+                    continue
+
+                if direct in self.__unknown_list:
+                    self.__unknown_wav.append(samples)
+                else:
+                    self.__label_all.append(direct)
+                    self.__all_wav.append([samples, direct])
+
+        self.__wav_all = np.reshape(np.delete(self.__all_wav, 1, 1), (len(self.__all_wav)))
+        self.__label_all = [i for i in np.delete(self.__all_wav, 0, 1).tolist()]
+
+        # Random pick start point
+
+    def get_one_noise(self, noise_num=0):
+        selected_noise = self.__background_noise[noise_num]
+        start_idx = random.randint(0, len(selected_noise) - 1 - 8000)
+        return selected_noise[start_idx:(start_idx + 8000)]
+
+    def preprocessing(self):
+        augment = 1
+        delete_index = []
+        for i in range(augment):
+            new_wav = []
+            noise = self.get_one_noise(i)
+            for i, s in enumerate(self.__wav_all):
+                if len(s) != 8000:
+                    delete_index.append(i)
+                    continue
+                s = s + (self.__max_ratio * noise)
+                self.__noised_wav.append(s)
+        np.delete(self.__wav_all, delete_index)
+        np.delete(self.__label_all, delete_index)
+        self.__wav_vals = np.array([x for x in self.__wav_all])
+        self.__label_vals = [x for x in self.__label_all]
+        self.__labels = copy.deepcopy(self.__label_vals)
+        for _ in range(augment):
+            self.__label_vals = np.concatenate((self.__label_vals, self.__labels), axis=0)
+        self.__label_vals = self.__label_vals.reshape(-1, 1)
+        # knowns audio random sampling
+        self.__unknown = self.__unknown_wav
+        np.random.shuffle(self.__unknown_wav)
+        self.__unknown = np.array(self.__unknown)
+        self.__unknown = self.__unknown[:2000 * (augment + 1)]
+        self.__unknown_label = np.array(['unknown' for _ in range(2000 * (augment + 1))])
+        self.__unknown_label = self.__unknown_label.reshape(2000 * (augment + 1), 1)
+        delete_index = []
+        for i, w in enumerate(self.__unknown):
+            if len(w) != 8000:
+                delete_index.append(i)
+        self.__unknown = np.delete(self.__unknown, delete_index, axis=0)
+        # silence audio
+        num_wav = (2000 * (augment + 1)) // len(self.__background_noise)
+        for i, _ in enumerate(self.__background_noise):
+            for _ in range((2000 * (augment + 1)) // len(self.__background_noise)):
+                self.__silence_wav.append(self.get_one_noise(i))
+        self.__silence_wav = np.array(self.__silence_wav)
+        self.__silence_label = np.array(['silence' for _ in range(num_wav * len(self.__background_noise))])
+        self.__silence_label = self.__silence_label.reshape(-1, 1)
+        self.__wav_vals = np.reshape(self.__wav_vals, (-1, 8000))
+        self.__noised_wav = np.reshape(self.__noised_wav, (-1, 8000))
+        self.__unknown = np.reshape(self.__unknown, (-1, 8000))
+        self.__silence_wav = np.reshape(self.__silence_wav, (-1, 8000))
+        print(self.__wav_vals.shape)
+        print(self.__noised_wav.shape)
+        print(self.__unknown.shape)
+        print(self.__silence_wav.shape)
+        print(self.__label_vals.shape)
+        print(self.__unknown_label.shape)
+        print(self.__silence_label.shape)
+        self.__wav_vals = np.concatenate((self.__wav_vals, self.__noised_wav), axis=0)
+        self.__wav_vals = np.concatenate((self.__wav_vals, self.__unknown), axis=0)
+        self.__wav_vals = np.concatenate((self.__wav_vals, self.__silence_wav), axis=0)
+        self.__label_vals = np.concatenate((self.__label_vals, self.__unknown_label), axis=0)
+        self.__label_vals = np.concatenate((self.__label_vals, self.__silence_label), axis=0)
+        print(len(self.__wav_vals))
+        print(len(self.__label_vals))
+
+    def get_wav_vals(self):
+        return self.__wav_vals
+
+    def get_label_vals(self):
+        return self.__label_vals
+
+    def get_target_list(self):
+        return self.__target_list
