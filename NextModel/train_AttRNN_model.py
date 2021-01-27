d1a53b514e0281cf6df8763d4073391976d1655d
unknown <joasiawozna@gmail.com>
2021-01-27 15:49:35 +0100

    Change AttRNN model

--- NextModel/train_AttRNN_model.py
+++ NextModel/train_AttRNN_model.py
@@ -1,5 +1,9 @@
+import os
+
 import tensorflow as tf
 from pathlib import Path
+
+from tensorflow.keras.callbacks import LearningRateScheduler
 from sklearn.utils import class_weight
 
 gpu_devices = tf.config.experimental.list_physical_devices("GPU")
@@ -11,6 +15,26 @@ from tensorflow import keras
 from utils import word2index, get_model, prepare_data, get_data, batch_generator, \
     get_noises, get_data_and_classes, batch_simple_generator, get_specgram, get_melspecgram, get_spectrogram_shape, \
     get_log_specgram, get_melspecgram, get_simple_model, AttRNNSpeechModel, batch_AttRNN_generator
+
+import math
+
+import wandb
+from wandb.keras import WandbCallback
+
+
+
+def step_decay(epoch):
+    initial_lrate = 0.001
+    drop = 0.4
+    epochs_drop = 15.0
+    lrate = initial_lrate * math.pow(drop,
+                                     math.floor((1 + epoch) / epochs_drop))
+
+    if (lrate < 4e-5):
+        lrate = 4e-5
+
+    print('Changing learning rate to {}'.format(lrate))
+    return lrate
 
 
 def train(batch_size=8, epochs=50, model_dir_name='default_model_name', with_noise=True,
@@ -40,15 +64,15 @@ def train(batch_size=8, epochs=50, model_dir_name='default_model_name', with_noi
     print(class_weights)
 
     # shape = (128, 32)  # check shape that spectrogram returns
-    #shape = get_spectrogram_shape(spectrogram_function, train_data[0])  # or use this function to check
-    shape = (129, 124, 1)
+    # shape = get_spectrogram_shape(spectrogram_function, train_data[0])  # or use this function to check
+    #shape = (129, 124, 1)
     train_gen = batch_AttRNN_generator(train_data, train_classes, train_noises, batch_size=batch_size)
     valid_gen = batch_AttRNN_generator(valid_data, valid_classes, valid_noises, batch_size=batch_size)
 
     keras.backend.clear_session()
 
     # change function get_model to different if you want to change network model
-    #model = get_model(num_classes, shape=shape)
+    # model = get_model(num_classes, shape=shape)
     model = AttRNNSpeechModel(11)
     model.summary()
 
@@ -59,7 +83,8 @@ def train(batch_size=8, epochs=50, model_dir_name='default_model_name', with_noi
                                                        verbose=1,
                                                        save_best_only=False)
     csv_logger = tf.keras.callbacks.CSVLogger(model_path + 'training.log')
-
+    l_rate = LearningRateScheduler(step_decay)
+    wandb = WandbCallback()
     model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics=["accuracy"])
 
     history = model.fit_generator(
@@ -68,15 +93,16 @@ def train(batch_size=8, epochs=50, model_dir_name='default_model_name', with_noi
         steps_per_epoch=train_data.shape[0] // batch_size,
         validation_data=valid_gen,
         validation_steps=valid_data.shape[0] // batch_size,
-        callbacks=[check_pointer, csv_logger],
+        callbacks=[check_pointer, csv_logger, l_rate, wandb],
         class_weight=class_weights)
-
+    model.save(os.path.join(wandb.run.dir, "model.h5"))
 
 if __name__ == "__main__":
-    batch_size = 16
-    epochs = 25
-    model_dir_name = 'model_AttRNN/'
-    with_noise = False
+    wandb.init(project="szum", name="AttRNN with command(0.8, 1.2) and noise(0,2)")
+    batch_size = 32
+    epochs = 100
+    model_dir_name = 'model_AttRNN_with_noise/'
+    with_noise = True
     # choose spectrogram function [get_specgram, get_log_specgram, get_melspecgram, get_stft]
     spectrogram_function = get_specgram
     train(batch_size=batch_size, epochs=epochs, model_dir_name=model_dir_name, with_noise=with_noise,
